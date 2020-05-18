da09b79a8527385aaeb98dc144981bc0bb4863b7
礒部洸生 <isobekouki@isobekouseinoMacBook-Pro.local>
2020-05-19 05:10:48 +0900

    imgfile into /img

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //ボタンを押した時の処理
 function setImage(id){
-    var images = ['f0.png', 'f1.png','f2.png','f3.png','f4.png', 'f5.png'];
+    var images = ['img/f0.png', 'img/f1.png','img/f2.png','img/f3.png','img/f4.png', 'img/f5.png'];
     //画像はランダムで表示させたい
     var i = parseInt(Math.random() * 5);
     document.getElementById(id).src = images[i];
